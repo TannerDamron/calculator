2c20dac9d050d3fa1cc8643cc47c56eb1bc6bfad
Maddy Haldeman and Tanner Damron <maddy@>
2019-01-14 11:01:39 -0800

    create different functions for calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,20 @@ var add = function(num1, num2) {
 	return num1 + num2;
 };
 
+var sub = function(num1, num2) {
+	return num1 - num2;
+};
+
+var mul = function(num1, num2) {
+	return num1 * num2;
+};
+
+var div = function(num1, num2) {
+	return num1 / num2;
+};
+
 var num1 = parseInt(prompt("Enter a number:"));
 var num2 = parseInt(prompt("Enter another number:"));
+var result = div(num1, num2);
 
-alert(add(num1, num2));
+alert(result);
