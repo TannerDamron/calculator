dc7812c5c3337f116c8b35105a820ea2cf707bb0
Maddy Haldeman and Tanner Damron <tanner@>
2019-01-14 11:18:08 -0800

    create different functions for calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,18 @@ var num2 = parseInt(prompt("Enter another number:"));
 var result = div(num1, num2);
 
 alert(result);
+
+// var weight = parseInt(prompt("Enter your weight (kg):"));
+// var height = parseInt(prompt("Enter your height (m)"));
+//
+// var result = weight / (height * height);
+//
+// alert(result);
+
+
+
+// var cels = parseInt(prompt("Enter the temperature in celsius:"));
+//
+// var result = (cels * (9 / 5)) + 32;
+//
+// alert(result);
